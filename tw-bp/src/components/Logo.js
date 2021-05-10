73a7f81f4ca871e7edbaf5a76afc44766473f5b0
Eddcakes <eddadamson@gmail.com>
2021-05-10 23:11:28 +0100

    logo should link home

--- tw-bp/src/components/Logo.js
+++ tw-bp/src/components/Logo.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 import logo from '../assets/beer-pong.svg';
 
 export function Logo() {
   return (
     <div className='px-4'>
-      <img src={logo} alt='logo' className='w-16' />
+      <Link to='/'>
+        <img src={logo} alt='logo with link home' className='w-16' />
+      </Link>
     </div>
   );
 }

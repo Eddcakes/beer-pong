e7d0da5a1af39eb419731d844f47410daacd5dc6
Eddcakes <eddadamson@gmail.com>
2021-06-16 20:40:27 +0100

    Session management (#10)
    * add connect-pg-simple
    
    * add database session by connection string
    
    * tidyup
    
    * check env for prod

--- server/README.md
+++ server/README.md
@@ -1,7 +1,3 @@
-# basic backend for beer pong database
+### basic backend for beer pong database
 
-probably going to migrate to postgres soon, and start using an ORM
-
-## why
-
-quick start to play around with some of the pages with database links i may want to use
+Originally mariadb, now moved to postgres.
--- server/src/index.js
+++ server/src/index.js
@@ -2,24 +2,36 @@ import express from 'express';
 import morgan from 'morgan'; //logger
 import cors from 'cors';
 import session from 'express-session';
+import pgSession from 'connect-pg-simple';
 
 import { notFound, errorHandler } from './middlewares.js';
 import { api } from './api/index.js';
-// do i want to use helmet?
-// do i want to use knex?
+import { poolPromise } from './db.js';
 
 const app = express();
+
+const pgs = pgSession(session);
+
+const constring = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.PGDATABASE}`;
+
+const cookiesSecure = process.env.NODE_ENV.toLowerCase() === 'production';
 
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(morgan('common'));
 app.use(
   session({
+    store: new pgs({
+      conString: constring,
+      schemaName: process.env.DATABASE,
+      pool: poolPromise,
+    }),
     secret: process.env.SESSION_SECRET,
     saveUninitialized: true,
     resave: false,
     cookie: {
       httpOnly: true,
       maxAge: Number(process.env.SESSION_MAX_AGE),
+      secure: cookiesSecure,
     },
   })
 );
@@ -34,5 +46,5 @@ app.use(errorHandler);
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`);
+  console.log(`listening at ${process.env.DB_HOST}:${port}`);
 });

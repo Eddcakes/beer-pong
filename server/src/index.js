76be70d9bac365254542b2adc09b66876fde3ca5
Eddcakes <eddadamson@gmail.com>
2021-06-17 01:01:21 +0100

    only use build in production, add script to simulate production

--- server/src/index.js
+++ server/src/index.js
@@ -45,11 +45,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1', api);
 
-app.use(express.static(path.join(__dirname, 'build')));
+// in production use the build file, in dev run server & front end individually
+// space in package.json as:production means we have to trim
+if (process.env.NODE_ENV.toLowerCase().trim() === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);

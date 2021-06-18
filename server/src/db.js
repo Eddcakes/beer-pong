eb34481b0613a3083447b2003672159e57570cff
Eddcakes <eddadamson@gmail.com>
2021-06-18 10:37:10 +0100

    add ssl option to db connection

--- server/src/db.js
+++ server/src/db.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const sslProd = process.env.NODE_ENV === 'production' ? true : false;
+
 const db_settings = {
   user: process.env.PGUSER,
   host: process.env.DB_HOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: Number(process.env.DB_PORT),
+  ssl: sslProd,
 };
 
 export const poolPromise = new pg.Pool(db_settings);

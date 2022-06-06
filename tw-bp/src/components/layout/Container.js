febdd46e6cd4a4eb8f5ddc35c257e36d562a1ddc
Eddcakes <eddadamson@gmail.com>
2022-06-06 22:41:53 +0100

    update container to be main element

--- tw-bp/src/components/layout/Container.js
+++ tw-bp/src/components/layout/Container.js
@@ -3,8 +3,8 @@ import React from 'react';
 // helper to keep content a nice max-width
 export function Container({ maxW = '', maxH = '', children }) {
   return (
-    <div className={`container mx-auto p-6 space-y-4 ${maxW} ${maxH}`}>
+    <main className={`container mx-auto p-6 space-y-4 ${maxW} ${maxH}`}>
       {children}
-    </div>
+    </main>
   );
 }

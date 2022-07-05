a6ea88f0feccbccfbd46380b5e0709378f5b34d7
Eddcakes <eddadamson@gmail.com>
2022-07-05 23:32:26 +0100

    feat(Match): update the look of match card. needs data changes to display friendly dates

--- tw-bp/src/components/Match.js
+++ tw-bp/src/components/Match.js
@@ -1,51 +1,92 @@
 import { Link } from 'react-router-dom';
+import { CircleRight } from '../icons';
 
 export function Match({ details }) {
+  return (
+    <div className='shadow px-2 py-2 space-y-4'>
+      <div className='flex flex-col justify-between h-full'>
+        <MatchHeader
+          venue={details.venue}
+          tournamentGame={!!details.tournament_id}
+          stage={details.stage}
+        />
+        <MatchDetails
+          event={details.event}
+          home={{
+            id: details.home_id,
+            name: details.home_name,
+            cupsLeft: details.home_cups_left,
+          }}
+          away={{
+            id: details.away_id,
+            name: details.away_name,
+            cupsLeft: details.away_cups_left,
+          }}
+        />
+        <MatchFooter date={details.date} matchId={details.id} />
+      </div>
+    </div>
+  );
+}
+
+function MatchHeader({ venue, tournamentGame, stage }) {
+  return (
+    <div className='flex justify-between text-xs font-semibold'>
+      <div>{venue}</div>
+      <div>{tournamentGame ? stage : 'friendly'} </div>
+    </div>
+  );
+}
+
+function MatchFooter({ date, matchId }) {
+  return (
+    <div className='flex justify-between items-end'>
+      <div className='text-xs'>{date && formatDateString(date)}</div>
+      <Link
+        className='text-primary hover:text-primary-light'
+        to={`/games/${matchId}`}
+        title='match details'
+      >
+        <CircleRight role='img' aria-label='Open game details' />
+      </Link>
+    </div>
+  );
+}
+
+function MatchDetails({ event, home, away }) {
   const isWinner = (cupsLeft) => {
     return cupsLeft === 0 ? true : false;
   };
   return (
-    <div className='shadow px-4 pb-4 space-y-2'>
-      <div className='flex justify-between text-xs font-semibold'>
-        <div>{details.venue}</div>
-        <div>{details.tournament_id ? details.stage : 'friendly'} </div>
-      </div>
+    <>
       <div className='text-center'>
-        <div className='font-bold'>{details.event}</div>
-        <div className='text-xs'>
-          {details.date && formatDateString(details.date)}
-        </div>
+        <div className='font-bold'>{event || '-'}</div>
       </div>
-      <div className='grid grid-cols-2 text-center'>
+      <div className='pt-2 grid grid-cols-2 text-center'>
         <div>
           <Link
             className={`text-link-text underline relative ${
-              isWinner(details.away_cups_left) && 'psudo-winner'
+              isWinner(away.cupsLeft) && 'psudo-winner'
             }`}
-            to={`/player/${details.home_id}`}
+            to={`/player/${home.id}`}
           >
-            {details.home_name}
+            {home.name}
           </Link>
-          <div>{details.home_cups_left}</div>
+          <div>{home.cupsLeft}</div>
         </div>
         <div>
           <Link
             className={`text-link-text underline relative ${
-              isWinner(details.home_cups_left) && 'psudo-winner'
+              isWinner(home.cupsLeft) && 'psudo-winner'
             }`}
-            to={`/player/${details.away_id}`}
+            to={`/player/${away.id}`}
           >
-            {details.away_name}
+            {away.name}
           </Link>
-          <div>{details.away_cups_left}</div>
+          <div>{away.cupsLeft}</div>
         </div>
       </div>
-      <div className='text-right'>
-        <Link className='text-link-text underline' to={`/games/${details.id}`}>
-          details...
-        </Link>
-      </div>
-    </div>
+    </>
   );
 }
 
--- tw-bp/src/icons/index.js
+++ tw-bp/src/icons/index.js
@@ -1,5 +1,6 @@
 /* source of icons: https://heroicons.com/ */
 export * from './ChevronDown';
+export * from './CircleRight';
 export * from './Eye';
 export * from './EyeOff';
 export * from './Plus';

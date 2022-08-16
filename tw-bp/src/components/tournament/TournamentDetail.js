5772ac563e63c1c8cbe9567ab9de454e0af96841
Eddcakes <eddadamson@gmail.com>
2022-08-16 12:03:11 +0100

    refactor tournaments, move check into component to stop pointless requests

--- tw-bp/src/components/tournament/TournamentDetail.js
+++ tw-bp/src/components/tournament/TournamentDetail.js
@@ -4,32 +4,52 @@ import { fetchGamesByTournamentId, fetchTournamentById } from '../../queries';
 import { MatchGrid } from '../index';
 
 export function TournamentDetail({ id }) {
+  const {
+    data: tournament,
+    isLoading: isLoadingTournament,
+    isError: tournamentError,
+  } = useQuery(['tournamentsById', id], () => fetchTournamentById(id));
+
+  if (tournament?.error) {
+    return <div>{tournament.message}</div>;
+  }
+  return (
+    <div>
+      {isLoadingTournament ? (
+        <div>Loading details...</div>
+      ) : tournament.length > 0 ? (
+        <div>
+          <TournamentInfo tournament={tournament[0]} />
+          <TournamentGames id={id} />
+        </div>
+      ) : (
+        <div>Tournament does not exist</div>
+      )}
+      {tournamentError && <div>Error trying to fetch tournament {id}</div>}
+    </div>
+  );
+}
+
+function TournamentInfo({ tournament }) {
+  return (
+    <div>
+      <h2>{tournament.title}</h2>
+      <h2>{tournament.venue_title}</h2>
+      <div>{tournament.date}</div>
+    </div>
+  );
+}
+
+function TournamentGames({ id }) {
   const { data: games, isLoading: isLoadingGames } = useQuery(
     ['gamesByTournamentId', id],
     () => fetchGamesByTournamentId(id)
   );
-  const { data: tournament, isLoading: isLoadingTournament } = useQuery(
-    ['tournamentsById', id],
-    () => fetchTournamentById(id)
-  );
-
-  const isLoading = isLoadingGames || isLoadingTournament;
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
   return (
-    <div>
-      <div>This is Tournament: {id}</div>
-      <>
-        <div>
-          details:
-          <pre>{JSON.stringify(tournament, null, 2)}</pre>
-        </div>
-        <>
-          <h2 className='text-lg'>Games</h2>
-          <MatchGrid games={games} />
-        </>
-      </>
-    </div>
+    <>
+      <h2 className='text-lg'>Games</h2>
+      {isLoadingGames && <div>loading games...</div>}
+      {!isLoadingGames && <MatchGrid games={games} />}
+    </>
   );
 }
--- tw-bp/src/pages/Tournament.js
+++ tw-bp/src/pages/Tournament.js
@@ -1,4 +1,3 @@
-import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 
 import {
@@ -9,26 +8,16 @@ import {
   Button,
 } from '../components';
 import { usePageTitle } from '../hooks/usePageTitle';
-import { fetchTournamentById } from '../queries';
 
 export function Tournament() {
   const { tournamentId } = useParams();
   usePageTitle(`Tournament ${tournamentId ? `: ${tournamentId}` : 'list'}`);
-  const { data, isLoading } = useQuery(['tournamentsById', tournamentId], () =>
-    fetchTournamentById(tournamentId)
-  );
   return (
     <>
       <Header />
       <Container>
         {tournamentId ? (
-          isLoading ? (
-            'loading...'
-          ) : data.length > 0 ? (
-            <TournamentDetail id={tournamentId} />
-          ) : (
-            'This tournament does not exist!'
-          )
+          <TournamentDetail id={tournamentId} />
         ) : (
           <>
             <div className='text-center'>
@@ -41,8 +30,3 @@ export function Tournament() {
     </>
   );
 }
-
-/* 
-New tournament or new game button should look like this Group-hover element
-https://tailwindcss.com/docs/pseudo-class-variants
-*/
